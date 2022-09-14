364cd2d6b54bf30b9c660a55ea717f7aac8f4beb
Eyad <eyadalsaba1989@gmail.com>
2022-09-14 11:29:29 +0200

    upload assets and config Tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueMe: 'rgba(var(--color-blue) / 1.0)',
+        grayMe: 'rgba(var(--color-gray) / 1.0)',
+        blackBackground: 'rgba(var(--color-black-background) / 0.1)',
+        whiteBackground: 'rgba(var(--color-white-background) / 0.1)'
+      }
+    },
   },
   plugins: [],
 }
